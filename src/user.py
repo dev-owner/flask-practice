f68e4dfc4d0346baff675b5121dbba2e091011a0
s_jaewoo <yureka1112@gmail.com>
2020-09-11 23:44:02 +0900

    change entity persist way from in-memory to sqlite3

--- src/user.py
+++ src/user.py
@@ -53,6 +53,11 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
+        if User.find_by_username(data['username']):
+            return {
+                       "message": "A user with that username already exists"
+                   }, 400
+
         conn = sqlite3.connect('db/data.db')
         cursor = conn.cursor()
 
