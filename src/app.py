343c1cc4b4d8b09c97159e814676c034dfde4a29
s_jaewoo <yureka1112@gmail.com>
2020-09-20 10:39:42 +0900

    apply sqlalchemy #5 - create table before first request

--- src/app.py
+++ src/app.py
@@ -7,12 +7,17 @@ from apis.user import UserRegister
 from src.security import authenticate, identity
 from database.db import db
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret'
 api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
